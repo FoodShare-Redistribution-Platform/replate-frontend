0fc94d37dcbba971e5fccd2aad31f8920f2359b3
parvathy2907 <parvathy896@gmail.com>
2026-03-09 17:24:37 +0530

    deployment

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/admin';
-
+//const API_URL = 'http://localhost:5001/api/admin';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/admin`;
 const authHeader = (token) => ({
     headers: { Authorization: `Bearer ${token}` },
 });
